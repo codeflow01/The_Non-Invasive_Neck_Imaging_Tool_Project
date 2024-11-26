2bd4642043f0f976a1b667c1c0b4513e7f5f0711
codeflow01 <codinghollywood@gmail.com>
2024-11-27 10:59:38 +1300

    Initialize basic index page

--- ImagingToolProject/app/(tabs)/index.tsx
+++ ImagingToolProject/app/(tabs)/index.tsx
@@ -1,15 +1,21 @@
-import { Text, View } from "react-native";
+import { View, Text } from "react-native";
+import { TouchableOpacity } from "react-native";
+import { useState } from "react";
 
 export default function Index() {
+  const [isPressed, setIsPressed] = useState<boolean>(false);
+
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View className="flex-1 items-center justify-center">
+      <TouchableOpacity onPress={() => setIsPressed(!isPressed)}>
+        <Text
+          className={`font-bold ${
+            isPressed ? "text-blue-600" : "text-red-700"
+          } ${isPressed ? "text-4xl" : "text-2xl"}`}
+        >
+          {isPressed ? "GOOD LUCK!" : "NECK IMAGE PROJECT"}
+        </Text>
+      </TouchableOpacity>
     </View>
   );
 }
