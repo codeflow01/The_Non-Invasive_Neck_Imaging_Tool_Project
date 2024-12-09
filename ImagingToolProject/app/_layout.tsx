8fd24e10f0fe2b72040a8438b7275970b3c91d57
codeflow01 <codinghollywood@gmail.com>
2024-12-09 22:43:02 +1300

    Set up Tamagui

--- ImagingToolProject/app/_layout.tsx
+++ ImagingToolProject/app/_layout.tsx
@@ -1,14 +1,25 @@
 import { Stack } from "expo-router";
 import "../global.css";
-import { PaperProvider } from "react-native-paper";
+import { TamaguiProvider, createTamagui } from "tamagui";
+import { config } from "@tamagui/config";
+// import { PaperProvider } from "react-native-paper";
+
+const tamaguiConfig = createTamagui(config);
+
+type Conf = typeof tamaguiConfig;
+declare module "@tamagui/core" {
+  interface TamaguiCustomConfig extends Conf {}
+}
 
 export default function RootLayout() {
   return (
-    <PaperProvider>
+    <TamaguiProvider config={tamaguiConfig}>
+      {/* <PaperProvider> */}
       <Stack>
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
         <Stack.Screen name="+not-found" />
       </Stack>
-    </PaperProvider>
+      {/* </PaperProvider> */}
+    </TamaguiProvider>
   );
 }
