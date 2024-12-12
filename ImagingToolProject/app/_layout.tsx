56ee539e8b358a6dedcc1ffa10e94337ce25ba75
codeflow01 <codinghollywood@gmail.com>
2024-12-13 11:57:08 +1300

    Complete Tamagui setup

--- ImagingToolProject/app/_layout.tsx
+++ ImagingToolProject/app/_layout.tsx
@@ -1,29 +1,35 @@
 import { Stack } from "expo-router";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import "../global.css";
-import { TamaguiProvider, createTamagui } from "tamagui";
-import { config } from "@tamagui/config";
+import {
+  DarkTheme,
+  DefaultTheme,
+  ThemeProvider,
+} from "@react-navigation/native";
+import { useColorScheme } from "react-native";
+import { TamaguiProvider } from "tamagui";
+import tamaguiConfig from "../tamagui.config";
+
 // import { PaperProvider } from "react-native-paper";
 
 const queryClient = new QueryClient();
 
-const tamaguiConfig = createTamagui(config);
-
-type Conf = typeof tamaguiConfig;
-declare module "@tamagui/core" {
-  interface TamaguiCustomConfig extends Conf {}
-}
-
 export default function RootLayout() {
+  const colorScheme = useColorScheme();
+
   return (
     <QueryClientProvider client={queryClient}>
-      <TamaguiProvider config={tamaguiConfig}>
-        {/* <PaperProvider> */}
-        <Stack>
-          <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-          <Stack.Screen name="+not-found" />
-        </Stack>
-        {/* </PaperProvider> */}
+      <TamaguiProvider config={tamaguiConfig} defaultTheme={colorScheme!}>
+        <ThemeProvider
+          value={colorScheme === "dark" ? DarkTheme : DefaultTheme}
+        >
+          {/* <PaperProvider> */}
+          <Stack>
+            <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+            <Stack.Screen name="+not-found" />
+          </Stack>
+          {/* </PaperProvider> */}
+        </ThemeProvider>
       </TamaguiProvider>
     </QueryClientProvider>
   );
--- ImagingToolProject/babel.config.js
+++ ImagingToolProject/babel.config.js
@@ -1,15 +1,32 @@
+const plugin = require("tailwindcss");
+
 module.exports = function (api) {
-    api.cache(true);
-    return {
-      presets: [
-        ["babel-preset-expo", { jsxImportSource: "nativewind" }],
-        "nativewind/babel",
-      ],
-      env: {
-        production: {
-          plugins: ["react-native-paper/babel"],
+  api.cache(true);
+  return {
+    presets: [
+      ["babel-preset-expo", { jsxImportSource: "nativewind" }],
+      "nativewind/babel",
+    ],
+    plugins: [
+      // Tamagui plugin
+      [
+        "@tamagui/babel-plugin",
+        {
+          components: ["tamagui"],
+          config: "./tamagui.config.ts",
+          logTimings: true,
+          disableExtraction: process.env.NODE_ENV === "development",
         },
+      ],
+      "react-native-reanimated/plugin",
+      ...(process.env.NODE_ENV === "production"
+        ? ["react-native-paper/babel"]
+        : []),
+    ],
+    env: {
+      production: {
+        plugins: ["react-native-paper/babel"],
       },
-    };
+    },
   };
-  
+};
