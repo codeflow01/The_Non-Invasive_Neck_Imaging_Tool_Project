94c501e18385569ca4a82e28140c37232b2631a2
codeflow01 <codinghollywood@gmail.com>
2025-01-09 11:13:43 +1300

    Update bottom tab UI

--- ImagingToolProject/app/(tabs)/_layout.tsx
+++ ImagingToolProject/app/(tabs)/_layout.tsx
@@ -1,16 +1,61 @@
 import { Tabs } from "expo-router";
 import FontAwesome5 from "@expo/vector-icons/FontAwesome5";
+import { Dimensions } from "react-native";
 
 export default function TabLayout() {
+  const screenWidth = Dimensions.get("window").width;
+  const screenHeight = Dimensions.get("window").height;
+  const horizontalPadding = screenWidth * 0.08;
+  const bottomPadding = screenHeight * 0.04;
+  const tabBarHeight = screenHeight * 0.08;
+  const tabBarWidth = screenWidth * 0.85;
+
   return (
-    <Tabs screenOptions={{ tabBarActiveTintColor: "#001e57" }}>
+    <Tabs
+      screenOptions={{
+        tabBarActiveTintColor: "#001e57",
+        tabBarInactiveTintColor: "#9E9E9E",
+        tabBarStyle: {
+          height: tabBarHeight,
+          width: tabBarWidth,
+          paddingBottom: screenHeight * 0.01,
+          paddingTop: screenHeight * 0.01,
+          backgroundColor: "white",
+          position: "relative",
+          bottom: bottomPadding,
+          alignSelf: "center",
+          justifyContent: "center",
+          alignItems: "center",
+          borderRadius: screenWidth * 0.04,
+          borderTopWidth: 0,
+          elevation: 3,
+          shadowColor: "#000",
+          shadowOffset: {
+            width: 0,
+            height: 2,
+          },
+          shadowOpacity: 0.1,
+          shadowRadius: 3,
+        },
+        tabBarLabelStyle: {
+          fontSize: screenWidth * 0.03,
+          marginTop: screenHeight * 0.005,
+        },
+        tabBarIconStyle: {
+          width: screenWidth * 0.06,
+          height: screenWidth * 0.06,
+        },
+      }}
+    >
+      {/* <Tabs screenOptions={{ tabBarActiveTintColor: "#001e57" }}> */}
+
       <Tabs.Screen
         name="index"
         options={{
-          title: "Home",
+          title: "Formula",
           headerShown: false,
           tabBarIcon: () => (
-            <FontAwesome5 name="home" size={24} color="#001e57" />
+            <FontAwesome5 name="ello" size={24} color="#001e57" />
           ),
         }}
       />
@@ -18,7 +63,7 @@ export default function TabLayout() {
       <Tabs.Screen
         name="imaging"
         options={{
-          title: "Imaging",
+          title: "Vision",
           headerShown: false,
           tabBarIcon: () => (
             <FontAwesome5 name="cloudsmith" size={24} color="#001e57" />
@@ -27,6 +72,17 @@ export default function TabLayout() {
       />
 
       <Tabs.Screen
+        name="gamma"
+        options={{
+          title: "Insight",
+          headerShown: false,
+          tabBarIcon: () => (
+            <FontAwesome5 name="chart-line" size={24} color="#001e57" />
+          ),
+        }}
+      />
+
+      {/* <Tabs.Screen
         name="beta"
         options={{
           title: "Python_Server",
@@ -35,18 +91,8 @@ export default function TabLayout() {
             <FontAwesome5 name="connectdevelop" size={24} color="#001e57" />
           ),
         }}
-      />
+      /> */}
 
-      <Tabs.Screen
-        name="gamma"
-        options={{
-          title: "Test_Gamma",
-          headerShown: false,
-          tabBarIcon: () => (
-            <FontAwesome5 name="connectdevelop" size={24} color="#001e57" />
-          ),
-        }}
-      />
     </Tabs>
   );
 }
--- ImagingToolProject/app/(tabs)/beta.tsx
+++ ImagingToolProject/app/(tabs)/beta.tsx
@@ -1,62 +1,62 @@
-import { View, Text } from "react-native";
-import { TouchableOpacity } from "react-native";
-import { useState } from "react";
-import { useQuery, useQueryClient } from "@tanstack/react-query";
+// import { View, Text } from "react-native";
+// import { TouchableOpacity } from "react-native";
+// import { useState } from "react";
+// import { useQuery, useQueryClient } from "@tanstack/react-query";
 
-export default function Beta() {
-  const [isPressed, setIsPressed] = useState<boolean>(false);
-  const queryClient = useQueryClient();
+// export default function Beta() {
+//   const [isPressed, setIsPressed] = useState<boolean>(false);
+//   const queryClient = useQueryClient();
 
-  const { data, isLoading, error, refetch } = useQuery({
-    queryKey: ["apiMessage"],
-    queryFn: async () => {
-      // Home
-      // const response = await fetch("http://192.168.1.19:8000/api");
-      // ABI
-      const response = await fetch("http://172.23.96.207:8000/api");
-      if (!response.ok) {
-        throw new Error("Network response was not ok");
-      }
-      return response.json();
-    },
-    enabled: isPressed,
-  });
+//   const { data, isLoading, error, refetch } = useQuery({
+//     queryKey: ["apiMessage"],
+//     queryFn: async () => {
+//       // Home
+//       // const response = await fetch("http://192.168.1.19:8000/api");
+//       // ABI
+//       const response = await fetch("http://172.23.96.207:8000/api");
+//       if (!response.ok) {
+//         throw new Error("Network response was not ok");
+//       }
+//       return response.json();
+//     },
+//     enabled: isPressed,
+//   });
 
-  const handlePress = () => {
-    if (isPressed) {
-      queryClient.removeQueries({ queryKey: ["apiMessage"] });
-    } else {
-      refetch();
-    }
-    setIsPressed(!isPressed);
-  };
+//   const handlePress = () => {
+//     if (isPressed) {
+//       queryClient.removeQueries({ queryKey: ["apiMessage"] });
+//     } else {
+//       refetch();
+//     }
+//     setIsPressed(!isPressed);
+//   };
 
-  return (
-    <View className="flex-1 items-center justify-center">
-      <TouchableOpacity onPress={handlePress}>
-        <Text
-          className={`font-bold bg-neutral-300 p-2 rounded-lg shadow-sm text-center ${
-            isPressed ? "text-red-700" : "text-[#001e57]"
-          } ${isPressed ? "text-2xl" : "text-2xl"}`}
-        >
-          {isPressed
-            ? "Press To Test Again"
-            : "Press to Test\n REST API Connection"}
-        </Text>
-      </TouchableOpacity>
-      {isLoading && (
-        <Text className="mt-20 text-red-700 text-2xl">Loading...</Text>
-      )}
-      {error && (
-        <Text className="mt-20 text-red-700 text-2xl">
-          Error: {error instanceof Error ? error.message : "Unknown error"}
-        </Text>
-      )}
-      {data && (
-        <Text className="mt-20 text-[#001e57] text-2xl text-center">
-          REST API Status: {data.message}
-        </Text>
-      )}
-    </View>
-  );
-}
+//   return (
+//     <View className="flex-1 items-center justify-center">
+//       <TouchableOpacity onPress={handlePress}>
+//         <Text
+//           className={`font-bold bg-neutral-300 p-2 rounded-lg shadow-sm text-center ${
+//             isPressed ? "text-red-700" : "text-[#001e57]"
+//           } ${isPressed ? "text-2xl" : "text-2xl"}`}
+//         >
+//           {isPressed
+//             ? "Press To Test Again"
+//             : "Press to Test\n REST API Connection"}
+//         </Text>
+//       </TouchableOpacity>
+//       {isLoading && (
+//         <Text className="mt-20 text-red-700 text-2xl">Loading...</Text>
+//       )}
+//       {error && (
+//         <Text className="mt-20 text-red-700 text-2xl">
+//           Error: {error instanceof Error ? error.message : "Unknown error"}
+//         </Text>
+//       )}
+//       {data && (
+//         <Text className="mt-20 text-[#001e57] text-2xl text-center">
+//           REST API Status: {data.message}
+//         </Text>
+//       )}
+//     </View>
+//   );
+// }
